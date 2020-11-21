0d4f6567eb9a51a8bb039c1cad776fa673dcabbe
Divesh Arora <diveshngp@gmail.com>
2020-11-21 20:50:54 +0530

    backend integration

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var mongoose=require('mongoose');
 var passport=require('passport');
 var LocalStrategy=require('passport-local');
 var User=require('./models/user');
+var Hospital=require('./models/hospital.js');
 var nodemailer = require('nodemailer');
 mongoose.connect("mongodb+srv://divesh:dev123456789@cluster0.l6u2q.mongodb.net/covid_care?retryWrites=true&w=majority",{ useNewUrlParser: true , useUnifiedTopology: true });
 
@@ -19,7 +20,10 @@ app.use(require("express-session")({
 
 app.use(passport.initialize());
 app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()) );
+passport.use(new LocalStrategy({
+    usernameField:'email',
+    passwordField:'password'
+},User.authenticate()) );
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -40,3 +44,59 @@ app.get("/login",function(req,res,next){
 app.post('/login',passport.authenticate('local',{failureRedirect:'/login'}),function(req,res){
 	res.redirect('/');
 });
+app.get("/reghos",function(req,res,next){
+	res.render("reghos.ejs");
+});
+app.post("/reghos",function(req,res){
+    var newhosp={name:req.body.name,pattern:req.body.pattern,address:req.body.address,phone:req.body.phone,beds:req.body.beds,ventilators:req.body.ventilators};
+   
+    Hospital.create(newhosp,function(err,newlycreated){
+		if(err)
+			console.log(err);
+		else{
+			console.log(newlycreated);
+			res.redirect("/");
+            
+		}
+			
+	});
+});
+
+app.get("/signup",function(req,res){
+    Hospital.find({},function(err,hospitals){
+        if(err)
+        console.log(err);
+        else
+        res.render('signup.ejs',{currentUser:req.user,hospitals:hospitals});
+    });
+
+});
+app.post("/signup",function(req,res){
+	var newUser= new User({username:req.body.username,email:req.body.email});
+	User.register(newUser,req.body.password,function(err,user){
+		if(err)
+			{
+				console.log(err);
+				res.redirect('/signup');
+			}
+		else{
+			var mailOptions = {
+				from: 'covidcare96@gmail.com',
+				to:req.body.email ,
+				subject: 'Sign Up Confirmation',
+                text: "You can now access the portal."
+			  };
+			  
+ transporter.sendMail(mailOptions, function(error, info){
+	if (error) {
+	  console.log(error);
+	} else {
+	  console.log('Email sent: ' + info.response);
+	}
+  });
+  
+			res.redirect('/login');
+		}
+	});
+});
+
